d394d2c8e79988d2c7e72547195f9090e1cbc0f4
Ubuntu <ubuntu@ip-172-31-1-53.ap-northeast-2.compute.internal>
2021-02-03 05:42:38 +0000

    route정리, 모듈별로 정리

--- app.js
+++ app.js
@@ -1,53 +1,32 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-const cors = require('cors');
+import express from 'express';
+import path from 'path;
+import morgan from 'morgan'; // HTTP REQUEST LOGGER
+import bodyParser from 'body-parser'; // PARSE HTML BODY
+import createError from 'http-erros';
+import session from 'express-session';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var chatRouter = require('./routes/chat');
-//var socket = require('./routes/socketio');
-var testAPIRouter = require("./routes/testAPI");
+import api from './routes';
 
-var app = express();
+const app = express();
+app.set('port', process.env.PORT || 3000); //port setup
 
+/* maraidb connection*/
 const maria = require('./maria');
 maria.connect();
 
-/* setup routers & static directory */
-//import api from './routes';
-//app.use('/api', api);
+app.use('/', express.static(path.join(__dirname, '../client/public')));
 
-// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set("view engine", "ejs");
-//app.engine("html", require('ejs').renderFile);
+/* set up routers & static directory */
+app.use('/api', api);
 
-//port setup
-app.set('port', process.env.PORT || 3000);
-
-//get home page
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/public/index.html'));
 });
 
-app.use(cors())
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use("/testAPI", testAPIRouter);
-
-// catch 404 and forward to error handler
+/* catch 404 & forward error to error handler*/
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
-// error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
@@ -58,10 +37,31 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-
-// ------- creates Server -------
-
-module.exports = app;
+/* create server */
 var server = app.listen(app.get('port'), function() {
-	console.log('Express server listening on port ' + server.address().port);
+        console.log('Express server listening on port ' + server.address().port);
 });
+//module.exports = app;
+
+
+//var createError = require('http-errors');
+//var express = require('express');
+//var path = require('path');
+//var logger = require('morgan');
+//var cors = require('cors');
+
+// view engine setup
+//app.set('views', path.join(__dirname, 'views'));
+//app.set("view engine", "ejs");
+//app.engine("html", require('ejs').renderFile);
+
+//app.use(cors())
+//app.use(logger('dev'));
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
+//app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
+//app.use("/testAPI", testAPIRouter);
+
