2d1d64688502b21e0df60ab738554a074478aa8a
lgaeun <55012143+lgaeun@users.noreply.github.com>
2021-02-03 15:23:59 +0900

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ import morgan from 'morgan'; // HTTP REQUEST LOGGER
 import bodyParser from 'body-parser'; // PARSE HTML BODY
 import createError from 'http-erros';
 import session from 'express-session';
+import cors from 'cors';
 
-import api from './routes';
+import route from './routes';
 
 const app = express();
 app.set('port', process.env.PORT || 3000); //port setup
@@ -14,26 +15,23 @@ app.set('port', process.env.PORT || 3000); //port setup
 const maria = require('./maria');
 maria.connect();
 
-app.use('/', express.static(path.join(__dirname, '../client/public')));
-
+//app.use('/', express.static(path.join(__dirname, '../client/public')));
 /* set up routers & static directory */
-app.use('/api', api);
-
+app.use('/', route);
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/public/index.html'));
 });
 
-/* catch 404 & forward error to error handler*/
+/* catch 404 & forward to error handler*/
 app.use(function(req, res, next) {
   next(createError(404));
 });
+/* handle error */
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
+  res.status(err.status || 500);  // render the error page
   res.render('error');
 });
 
@@ -41,14 +39,8 @@ app.use(function(err, req, res, next) {
 var server = app.listen(app.get('port'), function() {
         console.log('Express server listening on port ' + server.address().port);
 });
-//module.exports = app;
+module.exports = app;
 
-
-//var createError = require('http-errors');
-//var express = require('express');
-//var path = require('path');
-//var logger = require('morgan');
-//var cors = require('cors');
 
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
@@ -60,8 +52,3 @@ var server = app.listen(app.get('port'), function() {
 //app.use(express.json());
 //app.use(express.urlencoded({ extended: false }));
 //app.use(express.static(path.join(__dirname, 'public')));
-
-//app.use('/', indexRouter);
-//app.use('/users', usersRouter);
-//app.use("/testAPI", testAPIRouter);
-
