cc2c96f9b8621fe2fff776b790308822cd812b4c
lgaeun <gaeungaeun4837@gmail.com>
2021-02-05 05:31:01 +0000

    프론트-백 연결: client side rendering

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 var http = require('http');
 var createError = require('http-errors');
@@ -7,26 +6,34 @@ var path = require('path');
 var logger = require('morgan');
 var cors = require('cors');
 
-//import route from './routes';
 
 const app = express();
-app.set('port', process.env.PORT || 3000); //port setup
+//app.set('port', process.env.PORT || 3000); //port setup
 
 /* maraidb connection*/
 const maria = require('./maria');
 maria.connect();
 
-//app.use('/', express.static(path.join(__dirname, '../client/public')));
-/* set up routers & static directory */
-//app.use('/api', api);
+const api = require('./routes');
 
-const home = require('./routes/index');
-app.use('/', home);
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+//app.use(express.static(path.join(__dirname, '../client/public')));
+
+/* set up routers & static directory */
+app.use('/api', api);
 
 //app.get('*', (req, res) => {
-//    res.sendFile(path.resolve(__dirname, '../client/public/index.html'));
+//    res.sendFile(path.resolve(__dirname, '../client/src/index.js'));
 //});
+app.use(express.static(path.join(__dirname, '../client/public')));
 
+/* port listening */
+app.listen(3000, () =>{
+   console.log('Express server listening on port 3000');
+});
 
 /* catch 404 & forward to error handler*/
 app.use(function(req, res, next) {
@@ -41,19 +48,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-/* create server */
-var server = app.listen(app.get('port'), function() {
-        console.log('Express server listening on port ' + server.address().port);
-});
 module.exports = app;
-
-// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set("view engine", "ejs");
-//app.engine("html", require('ejs').renderFile);
-
-//app.use(cors())
-//app.use(logger('dev'));
-//app.use(express.json());
-//app.use(express.urlencoded({ extended: false }));
-//app.use(express.static(path.join(__dirname, 'public')));
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
-import chat from './chat.js';
-import users from './users.js';
-import testAPI from './testAPI.js';
-import socket from './socketio.js';
+const chat = require('./chat.js');
+const users = require ('./users.js');
+const testAPI = require ('./testAPI.js');
+//const socket = require ('./socketio.js');
 
 const express = require('express');
 const path = require('path');
@@ -10,11 +10,12 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	console.log('메인페이지가 로드되었습니다');
-	res.sendFile(path.resolve(__dirname, '../client/public/index.html');
+	res.send(express.static(path.join(__dirname, '../../client/public/index.html')));
+	//	res.render('../client/src/index.js');
+	//res.sendFile(path.resolve(__dirname, '../client/public/index.html');
 });
-
 router.use('/chat', chat);
-router.use('/socket', socket);
+//router.use('/socket', socket);
 router.use('/users', users);
 router.use('/testAPI', testAPI);
 
